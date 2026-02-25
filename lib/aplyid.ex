51eca11dc50550ad4acb41f8b0ab4fca20d24434
Colin Harris <col.w.harris@gmail.com>
2026-02-25 14:24:30 +1100

    initial commit

---
+++ lib/aplyid.ex
@@ -0,0 +1,18 @@
+defmodule Aplyid do
+  @moduledoc """
+  Documentation for `Aplyid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aplyid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aplyid_test.exs
@@ -0,0 +1,8 @@
+defmodule AplyidTest do
+  use ExUnit.Case
+  doctest Aplyid
+
+  test "greets the world" do
+    assert Aplyid.hello() == :world
+  end
+end
